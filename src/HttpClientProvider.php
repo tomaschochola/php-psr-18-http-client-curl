07acffecc6f1014e2d48e5f4bfb5be6960a4f965
Tomáš Chochola <tomaschochola@tomaschochola.cz>
2026-03-08 05:11:17 +0100

    Changes of the day 2026-03-08T05:11:17+01:00

--- src/HttpClientProvider.php
+++ src/HttpClientProvider.php
@@ -1,16 +1,5 @@
 <?php
 
-/**
- * @author Tomáš Chochola <tomaschochola@tomaschochola.cz>
- * @copyright © 2026 Tomáš Chochola <tomaschochola@tomaschochola.cz>
- *
- * @license CC-BY-ND-4.0
- *
- * @see {@link https://creativecommons.org/licenses/by-nd/4.0/} License
- * @see {@link https://github.com/tomaschochola} GitHub Profile
- * @see {@link https://github.com/sponsors/tomaschochola} GitHub Sponsors
- */
-
 declare(strict_types=1);
 
 namespace TomasChochola\Psr\Http\Client;
@@ -26,14 +15,13 @@ use Traversable;
  *
  * @implements IteratorAggregate<mixed, mixed>
  */
-readonly class HttpClientManifest implements IteratorAggregate
+readonly class HttpClientProvider implements IteratorAggregate
 {
     #[NoDiscard]
     #[Override]
     public function getIterator(): Traversable
     {
         yield CurlHttpClient::class => [CurlHttpClient::class, 'unload'];
-
         yield ClientInterface::class => [CurlHttpClient::class, 'unload'];
     }
 }
